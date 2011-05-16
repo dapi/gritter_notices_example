30361e2695ec05ca0b13c8d84f2836de1a932805
Danil Pismenny <danil@orionet.ru>
2011-05-16 13:36:04 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GritterNoticesExample::Application.config.secret_token = 'c7653acb4e253d298124ae2af0e49f907ab34fe26d62e17f52217d07176a95d5ef48caee6a64dd0167beca92ca10b2e2be3db459d9aa3a5a80e48e98431d8bec'
